6bc577f594474ffd32c4c44fe86a87dfff1cc3ac
Lokeshraj-m <finzly_lokesh@outlook.com>
2022-08-16 15:41:38 +0530

    initial commit

--- Assignment/src/classMethodCalling/CompoundInterest.java
+++ Assignment/src/classMethodCalling/CompoundInterest.java
@@ -2,6 +2,7 @@ package classMethodCalling;
 
 public class CompoundInterest {
 	public void compoundInterest(int P,double R, int n,int T ){
+		// P-Principal, R-Rate of interest in % per annum, n-number of times in a year, T-Time
 		R = R/100;
 		double C = 1+(R/n);
 		double ci = (P*Math.pow(C,(n*T)))-P;
