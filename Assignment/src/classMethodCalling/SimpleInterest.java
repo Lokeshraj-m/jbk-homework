b30a50d4f644dd8665f7c382c1575750494e4562
Lokeshraj-m <finzly_lokesh@outlook.com>
2022-08-16 16:49:01 +0530

    First commit

--- Assignment/src/classMethodCalling/SimpleInterest.java
+++ Assignment/src/classMethodCalling/SimpleInterest.java
@@ -2,8 +2,10 @@ package classMethodCalling;
 
 public class SimpleInterest {
 	public void simpleInterest(int P,float R,int T) {
+		// P-Principal, R-Rate of interest in % per annum, T-Time
 		R = R/100;
 		double I = P * R * T;
+		// I - Simple interest
 		System.out.println("Simple Interest = " +I);
 	}
 
